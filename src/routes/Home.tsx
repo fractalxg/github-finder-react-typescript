3c340850081c55cd06894e52ef73d0149537aa2e
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2023-11-29 20:01:59 -0300

    estilização de search bar e button

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -14,6 +14,18 @@ const Home = () => {
 
     const data = await res.json()
 
+    const {avatar_url, login, location, followers, following} = data
+
+    const userData: UserProps = {
+        avatar_url,
+        login,
+        location,
+        followers, 
+        following,
+    }
+
+    setUser(userData)
+    
     console.log(data)
 
   }
@@ -21,6 +33,7 @@ const Home = () => {
   return (
     <div>
         <Search loadUser={loadUser}/>
+        {user && <p>{user.login}</p>}
     </div>
   )
 }
