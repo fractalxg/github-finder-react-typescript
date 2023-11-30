e499a1b71d707f9eb87aa4a4b376f1b33ec8e759
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2023-11-29 21:27:50 -0300

    informações de usuário e erro caso não encontre

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,15 +2,21 @@ type SearchProps = {
     loadUser: (userName: string) => Promise<void>
 }
 
-import { useState } from "react"
+import { useState, KeyboardEvent } from "react"
 
-import {BsSearch} from "react-icons/bs"
+import { BsSearch } from "react-icons/bs"
 
 import classes from "./Search.module.css"
 
 const Search = ({loadUser}: SearchProps) => {
 
   const [userName, setUserName] = useState("")
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+        loadUser(userName)
+    }
+  }
 
   return (
     <div className={classes.search}>
@@ -21,6 +27,7 @@ const Search = ({loadUser}: SearchProps) => {
                 type="text" 
                 placeholder="Digite o nome do usuário" 
                 onChange={(e) => setUserName(e.target.value)}
+                onKeyDown={handleKeyDown}
             />
             <button onClick={() => loadUser(userName)}>
                 <BsSearch />
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,27 @@
 import { UserProps } from "../types/user"
 
 import Search from "../components/Search"
-
+import User from "../components/User"
 import { useState } from "react"
+import Error from "../components/Error"
 
 const Home = () => {
 
   const [user, setUser] = useState<UserProps | null>(null)
+  const [error, setError] = useState(false)
 
   const loadUser = async(userName: string) => {
+    setError(false)
+    setUser(null)
 
     const res = await fetch(`https://api.github.com/users/${userName}`)
 
     const data = await res.json()
+
+    if (res.status === 404) {
+      setError(true)
+      return
+    }
 
     const {avatar_url, login, location, followers, following} = data
 
@@ -25,7 +34,7 @@ const Home = () => {
     }
 
     setUser(userData)
-    
+
     console.log(data)
 
   }
@@ -33,7 +42,8 @@ const Home = () => {
   return (
     <div>
         <Search loadUser={loadUser}/>
-        {user && <p>{user.login}</p>}
+        {user && <User {...user}/>}
+        {error && <Error />}
     </div>
   )
 }
